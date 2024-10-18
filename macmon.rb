dbed121069b662e1b8c104d2081e28c344d0a37a
Vlad Pronsky <v.pronsky@gmail.com>
2024-10-18 18:44:19 +0300

    update formula

--- macmon.rb
+++ macmon.rb
@@ -2,14 +2,19 @@ class Macmon < Formula
   desc "Sudoless performance monitoring CLI tool for Apple Silicon processors"
   homepage "https://github.com/vladkens/macmon"
   version "0.3.0"
-  depends_on macos: :monterey
+  license "MIT"
 
-  on_arm do
-    url "https://github.com/vladkens/macmon/releases/download/v0.3.0/macmon-v0.3.0.tar.gz"
-    sha256 "ae42d998efc4329d9a53859f8a19bb26c649b07ed9c66414504ead98ead6bc9e"
+  depends_on arch: :arm64
+  depends_on :macos
 
-    def install
-      bin.install "macmon"
-    end
+  url "https://github.com/vladkens/macmon/releases/download/v0.3.0/macmon-v0.3.0.tar.gz"
+  sha256 "ae42d998efc4329d9a53859f8a19bb26c649b07ed9c66414504ead98ead6bc9e"
+
+  def install
+    bin.install "macmon"
+  end
+
+  test do
+    system "#{bin}/macmon", "-V"
   end
 end
