201a56100cead2e7b24823c590ee5126b83607cb
Vlad Pronsky <v.pronsky@gmail.com>
2025-04-26 23:53:25 +0300

    pin version

--- ecloop.rb
+++ ecloop.rb
@@ -1,9 +1,11 @@
 class Ecloop < Formula
   desc "High-performance tool for computing secp256k1 public keys with advanced search features"
   homepage "https://github.com/vladkens/ecloop"
-  url "https://github.com/vladkens/ecloop.git", :using => :git
-  version "0.3.0"
   license "MIT"
+  url "https://github.com/vladkens/ecloop.git",
+    tag:      "v0.3.0",
+    revision: "c9eb9ee07398cbc33f8a9aa5ffe0b03022814aae"
+  version "0.3.0"
 
   def install
     system "make"
