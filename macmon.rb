a0f3701e2d3c6ebc1591f94fe167a3e1092e7a79
vladkens <v.pronsky@gmail.com>
2026-05-02 15:42:50 +0300

    macmon 0.7.2

--- macmon.rb
+++ macmon.rb
@@ -1,14 +1,14 @@
 class Macmon < Formula
   desc "Sudoless performance monitoring CLI tool for Apple Silicon processors"
   homepage "https://github.com/vladkens/macmon"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/vladkens/macmon/releases/download/v0.7.1/macmon-v0.7.1.tar.gz"
-  sha256 "58742991eab3ae8efcefafeef01ae333a111ca2f02513f1769cb6596b2b0bfaf"
+  url "https://github.com/vladkens/macmon/releases/download/v0.7.2/macmon-v0.7.2.tar.gz"
+  sha256 "c79fdc7ab02b456b897dcc3ea041678420d7a1d5bd669aaac36fda3885572ad6"
 
   def install
     bin.install "macmon"
